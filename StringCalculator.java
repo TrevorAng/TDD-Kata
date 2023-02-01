7038a4b59fb873f9195b99e47d549c44edc39bf6
TrevorAng <99828808+TrevorAng@users.noreply.github.com>
2023-02-01 16:15:59 +0800

    Included exception for negative numbers

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class StringCalculator{
     public int Add(String numbers){
         int sum = 0;
-        String[] numbersArr = numbers.split(",");
+        numbers = "0," + numbers;
+        boolean negativeCheck = false;
+        ArrayList<Integer> negativeNum = new ArrayList<>();
+   
+        String[] numbersArr = numbers.split("[^\\d-]+");
+
 
         int size = numbersArr.length;
         int [] intArr = new int [size];
@@ -12,13 +18,26 @@ public class StringCalculator{
         }
 
         for(int i = 0;i<size;i++){
+            if (intArr[i]<0){
+                negativeCheck = true;
+                negativeNum.add(intArr[i]); 
+
+            }
             sum += intArr[i];
         }
-        return sum;
+
+        if (negativeCheck){
+            System.out.println(negativeNum);
+            return -1;
+        }
+        else{
+            return sum;
+        }
 
     }
 
     public static void main (String[] args){
+
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter a string of numbers:");
 
@@ -27,4 +46,4 @@ public class StringCalculator{
         StringCalculator sc = new StringCalculator();
         System.out.println(sc.Add(numbers));
     }
-}
+} 
