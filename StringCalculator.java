06cd291c1c2a55f770115ac75d5ea46aa994bbf2
TrevorAng <99828808+TrevorAng@users.noreply.github.com>
2023-02-01 23:27:54 +0800

    Implemented call count

--- StringCalculator.java
+++ StringCalculator.java
@@ -3,67 +3,76 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class StringCalculator{
-    public int Add(String numbers){
+public class StringCalculator {
+    int addCalledCount;
+
+    public int Add(String numbers) {
+        this.addCalledCount += 1;
+        
         int sum = 0;
+        // explanation for why we pad with 0
         numbers = "0," + numbers;
-        boolean negativeCheck = false;
-        ArrayList<Integer> negativeNum = new ArrayList<>();
-   
+
         String[] numbersArr = numbers.split("[^\\d-]+");
         int size = numbersArr.length;
-        int [] intArr = new int [size];
-        for(int i=0; i<size; i++) {
-           intArr[i] = Integer.parseInt(numbersArr[i]);
+        int[] intArr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            intArr[i] = Integer.parseInt(numbersArr[i]);
         }
 
-        for(int i = 0;i<size;i++){
-            if (intArr[i]<0){
+        boolean negativeCheck = false;
+        ArrayList<Integer> negativeNum = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            if (intArr[i] < 0) {
                 negativeCheck = true;
-                negativeNum.add(intArr[i]); 
+                negativeNum.add(intArr[i]);
             }
 
-            if (intArr[i]>1000){
-                intArr[i] = 0;
+            if (intArr[i] > 1000) {
+                continue;
             }
 
             sum += intArr[i];
         }
 
-        if (negativeCheck){
+        if (negativeCheck) {
+            // supposed to throw an exception
             System.out.println(negativeNum);
             return -1;
-        }
-        else{
+        } else {
             return sum;
         }
-
     }
 
+    public int GetCalledCount() {
+        return this.addCalledCount;
+    }
 
-
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter a string of numbers:");
 
-        String numbers = myObj.nextLine(); 
+        String numbers = myObj.nextLine();
         myObj.close();
         StringCalculator sc = new StringCalculator();
         System.out.println(sc.Add(numbers));
     }
 
     @Test
-    public void test(){
+    public void test() {
         StringCalculator sc = new StringCalculator();
-        assertEquals(sc.Add("1,2,3"),6);
-        assertEquals(sc.Add("3\n7"),10);
-        assertEquals(sc.Add("3\n7,6\n3"),19);
-        assertEquals(sc.Add("\\e3*g\n7,6\n3"),19);
-        assertEquals(sc.Add("//;\n1;2"),3);
-        assertEquals(sc.Add("//;\n10000;2,5"),7);
-        assertEquals(sc.Add("//[***]\n1***2***3"),6);
-        assertEquals(sc.Add("//[**][%%]\n1**2%%3"),6);
+        assertEquals(sc.Add("1,2,3"), 6);
+        assertEquals(sc.Add("3\n7"), 10);
+        assertEquals(sc.Add("3\n7,6\n3"), 19);
+        assertEquals(sc.Add("\\e3*g\n7,6\n3"), 19);
+        assertEquals(sc.Add("//;\n1;2"), 3);
+        assertEquals(sc.Add("//;\n10000;2,5"), 7);
+        assertEquals(sc.Add("//[***]\n1***2***3"), 6);
+        assertEquals(sc.Add("//[**][%%]\n1**2%%3"), 6);
+        assertEquals(sc.Add("//[delim1][delim2]\n"), 3);
 
     }
-} 
+}
