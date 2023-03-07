5741a3a08a0d57b269b0c1edb607725716dedb07
TrevorAng <99828808+TrevorAng@users.noreply.github.com>
2023-03-07 15:26:25 +0800

    Added Test for Empty String

---
+++ StringCalculator3.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StringCalculator3 {
+    public int Add(String numbers){
+        
+
+    
+
+        return 0;
+
+    }
+
+    @Test
+    public void forEmptyString() {
+        StringCalculator3 sc = new StringCalculator3();
+        assertEquals(sc.Add(""), 0);
+        assertEquals(sc.Add(""), 0);
+    
+
+    }
+
+}
