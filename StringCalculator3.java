b70b7e72d84b56fb8a1fb272b09a7714cc9c499d
TrevorAng <99828808+TrevorAng@users.noreply.github.com>
2023-03-07 15:42:03 +0800

    Added test for comma delimited string
    For integer values

--- StringCalculator3.java
+++ StringCalculator3.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 public class StringCalculator3 {
     public int Add(String numbers){
         
+        if (numbers == "") {
+            return 0;
+        }
 
     
 
@@ -16,8 +19,14 @@ public class StringCalculator3 {
     public void forEmptyString() {
         StringCalculator3 sc = new StringCalculator3();
         assertEquals(sc.Add(""), 0);
-        assertEquals(sc.Add(""), 0);
     
+    }
+
+    @Test
+    public void commaDelimitedString () {
+
+        StringCalculator3 sc = new StringCalculator3();
+        assertEquals(sc.Add("1,2,3"),6);
 
     }
 
