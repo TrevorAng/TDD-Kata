c83335a6be8986f46068b758fa035bf88a094270
TrevorAng <99828808+TrevorAng@users.noreply.github.com>
2023-03-07 15:48:19 +0800

    Added computation for comma delimited String test

--- StringCalculator3.java
+++ StringCalculator3.java
@@ -3,15 +3,18 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class StringCalculator3 {
-    public int Add(String numbers){
-        
+    public int Add(String numbers) {
+
         if (numbers == "") {
             return 0;
         }
 
-    
-
-        return 0;
+        String[] nums = numbers.split("[,\n]");
+        int sum = 0;
+        for (String num : nums) {
+            sum += Integer.parseInt(num);
+        }
+        return sum;
 
     }
 
@@ -19,14 +22,16 @@ public class StringCalculator3 {
     public void forEmptyString() {
         StringCalculator3 sc = new StringCalculator3();
         assertEquals(sc.Add(""), 0);
-    
+
     }
 
     @Test
-    public void commaDelimitedString () {
+    public void commaDelimitedString() {
 
         StringCalculator3 sc = new StringCalculator3();
-        assertEquals(sc.Add("1,2,3"),6);
+        assertEquals(sc.Add("1,2,3"), 6);
+        assertEquals(sc.Add("0,2,3"), 5);
+        assertEquals(sc.Add("5,3,2"), 10);
 
     }
 
