1d203f43a3922d88013053d09dc88e0a989bbd88
TrevorAng <99828808+TrevorAng@users.noreply.github.com>
2023-03-09 16:38:33 +0800

    Created new function for calculating and parsing integers

--- StringCalculator3.java
+++ StringCalculator3.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertEquals;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.Test;
 
 public class StringCalculator3 {
@@ -9,7 +10,36 @@ public class StringCalculator3 {
             return 0;
         }
 
-        String[] nums = numbers.split("[,\n]");
+        String[] nums;
+
+        if (numbers.startsWith("//")) {
+            nums = numbers.split(findDelimiter(numbers));
+            return calculate(nums);
+
+        }
+
+
+
+        nums = numbers.split("[,\n]");
+
+        return calculate(nums);
+
+    }
+
+    public String findDelimiter(String numbers) {
+        if (numbers.startsWith("//")) {
+            Pattern pattern = Pattern.compile("//(.+)\n");
+            Matcher matcher = pattern.matcher(numbers);
+            if (matcher.find()) {
+                String delimiter = matcher.group(1);
+                return "[" + Pattern.quote(delimiter) + "\n,]";
+            }
+        }
+        return "[,\n]";
+    }
+
+    public int calculate(String[] nums) {
+
         int sum = 0;
         for (String num : nums) {
             sum += Integer.parseInt(num);
@@ -41,5 +71,12 @@ public class StringCalculator3 {
         StringCalculator3 sc = new StringCalculator3();
         assertEquals(sc.Add("1\n2"),3);
         assertEquals(sc.Add("1\n2,3,4"), 10);
+    }
+
+    @Test
+    public void differentDelimiter() {
+
+        StringCalculator3 sc = new StringCalculator3();
+        assertEquals(sc.Add("//;\n1;2"),3);
     }
 }
